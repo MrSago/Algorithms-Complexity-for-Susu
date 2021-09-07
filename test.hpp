2d1108e5544ae71a6e34d2a772a090b5f57a907c
Alexander <37180898+MrSago@users.noreply.github.com>
2021-09-07 22:29:35 +0300

    refact

--- test.hpp
+++ test.hpp
@@ -13,23 +13,6 @@ constexpr int ARR_SIZE[] = {
     1000, 2000, 3500, 6000, 10000, 15000, 20000, 30000, 50000, 100000
 };
 
-using test_t = std::vector<std::pair<int, long long>>;
-
-
-template<typename T>
-test_t testSort(std::vector<T> (*gen_fun)(size_t), long long (*sort_fun)(std::vector<T>&)) {
-    test_t result;
-    for (auto& sz : ARR_SIZE) {
-        std::vector<T> arr = gen_fun(sz);
-        long long time = sort_fun(arr);
-        if (!checkSort(arr)) {
-            throw std::runtime_error("Array not sorted!");
-        }
-        result.push_back( { sz, time } );
-    }
-    return result;
-}
-
 
 template<typename T>
 void _print_vector(std::vector<T>& arr) {
@@ -40,8 +23,21 @@ void _print_vector(std::vector<T>& arr) {
 }
 
 
-void _test_print(test_t t) {
-    for (auto& it : t) {
+template<typename T>
+void _run_test(std::vector<T> (*gen_fun)(size_t), long long (*sort_fun)(std::vector<T>&)) {
+    std::vector<std::pair<int, long long>> result;
+
+    for (auto& sz : ARR_SIZE) {
+        std::vector<T> arr = gen_fun(sz);
+        long long time = sort_fun(arr);
+        if (!checkSort(arr)) {
+            throw std::runtime_error("Array not sorted!");
+        }
+        result.push_back( { sz, time } );
+    }
+
+    std::cout << "    " << std::setw(10) << std::left << "Size" << "Time (sec)\n";
+    for (auto& it : result) {
         std::cout << "    " << std::setw(10) << std::left << it.first << it.second * 1e-9 << '\n';
     }
     std::cout << '\n';
@@ -49,15 +45,7 @@ void _test_print(test_t t) {
 
 
 template<typename T>
-void _run_test(std::vector<T> (*gen_fun)(size_t), long long (*sort_fun)(std::vector<T>&)) {
-    std::cout << "    " << std::setw(10) << std::left << "Size" << "Time (sec)\n";
-    test_t test_res = testSort(gen_fun, sort_fun);
-    _test_print(test_res);
-}
-
-
-template<typename T>
-void startTestSort(std::string sort_name, long long (*sort_fun)(std::vector<T>&)) {
+void startTestSort(const char sort_name[], long long (*sort_fun)(std::vector<T>&)) {
     std::cout << "\n============ " << sort_name << " ============\n\n";
 
     std::cout << "Best case (increasing array):\n";
