d4a472a5b752f19608ccba210fdba2d3a2362ac9
Alexander <37180898+MrSago@users.noreply.github.com>
2021-09-10 21:24:59 +0300

    fix

--- perfomance.cpp
+++ perfomance.cpp
@@ -4,11 +4,12 @@
 
 
 int main () {
-    freopen("perfomance.txt", "w", stdout);
+    //freopen("perfomance.txt", "w", stdout);
     constexpr size_t cnt = (size_t)100e9;
     constexpr size_t ops = cnt * 3;
     size_t i = 0;
 
+    std::cerr << "Test started...";
     auto start = std::chrono::high_resolution_clock::now();
 
     while (i < cnt) {
@@ -16,11 +17,12 @@ int main () {
     }
 
     auto stop = std::chrono::high_resolution_clock::now();
+    std::cerr << "\nDone!\n";
 
     double t = (stop - start).count() * 1e-9;
-    std::cout << "time: " << t << "sec\n";
+    std::cout << "time: " << t << " sec\n";
     std::cout << "ops: " << ops << '\n';
-    std::cout << "perfomance: " << ops / t << " ops/sec\n";
+    std::cout << "perfomance: " << static_cast<size_t>(ops / t) << " ops/sec\n";
 
     return 0;
 }
