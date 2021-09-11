a8c4c1b24cd4c4aef4b5ae6f1addb955db0c11b4
Alexander <37180898+MrSago@users.noreply.github.com>
2021-09-11 05:21:58 +0300

    fix Floyd ops counter for parallel

--- lab5/Floyd.hpp
+++ lab5/Floyd.hpp
@@ -5,18 +5,26 @@
 #include <chrono>
 #include <omp.h>
 
+static size_t ops = 0;
+
 
 template<typename T>
 long long Floyd(T** w, int N) {
     auto start = std::chrono::high_resolution_clock::now();
 
     int k, i, j;
+    ops = 3;
     for (k = 0; k < N; ++k) {
         for (i = 0; i < N; ++i) {
             for (j = 0; j < N; ++j) {
-                w[i][j] = std::min(w[i][j], w[i][k] + w[k][j]);
+                if (w[i][k] != 0 && w[k][j] != 0) {
+                    w[i][j] = std::min(w[i][j], w[i][k] + w[k][j]);
+                }
+                ops += 23;
             }
+            ops += 5;
         }
+        ops += 5;
     }
 
     auto stop = std::chrono::high_resolution_clock::now();
@@ -31,13 +39,24 @@ long long FloydParallel(T** w, int N, int threads) {
 
     int k, i, j;
     omp_set_num_threads(threads);
-#pragma omp parallel for shared(w) private(k, i, j)
-    for (k = 0; k < N; ++k) {
-        for (i = 0; i < N; ++i) {
-            for (j = 0; j < N; ++j) {
-                w[i][j] = std::min(w[i][j], w[i][k] + w[k][j]);
+    #pragma omp parallel shared(w) private(k, i, j)
+    {
+        size_t ops_th = 3;
+        #pragma omp for
+        for (k = 0; k < N; ++k) {
+            for (i = 0; i < N; ++i) {
+                for (j = 0; j < N; ++j) {
+                    if (w[i][k] != 0 && w[k][j] != 0) {
+                        w[i][j] = std::min(w[i][j], w[i][k] + w[k][j]);
+                    }
+                    ops_th += 23;
+                }
+                ops_th += 5;
             }
+            ops_th += 5;
         }
+        #pragma omp atomic
+        ops += ops_th;
     }
 
     auto stop = std::chrono::high_resolution_clock::now();
